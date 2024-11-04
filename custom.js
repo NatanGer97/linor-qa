03fa4b22ff22f5f22f4a9264d57a72152ee4751b
NatanGer97 <Natanger97@gmail.com>
2024-11-04 20:01:51 +0200

    change

--- custom.js
+++ custom.js
@@ -1,57 +1,25 @@
-let currentSectionIndex = 0;
 const sections = document.querySelectorAll('.section');
-const totalSections = sections.length;
-let isScrolling = false; // Prevent multiple scrolls
 
-function scrollToSection(index) {
-    if (index < 0 || index >= totalSections || isScrolling) return;
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('.nav-link');
+    const sections = document.querySelectorAll('.section');
 
-    isScrolling = true; // Disable further scrolling
-    sections[index].scrollIntoView({ behavior: 'smooth' });
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            const targetIndex = Array.from(sections).findIndex(section => section.id === targetId);
+            updateURLHash(targetIndex);
 
-    setTimeout(() => {
-        isScrolling = false; // Re-enable scrolling after animation
-    }, 1000); // Adjust the timeout to match the animation duration
-
-    currentSectionIndex = index;
-    updateActiveNavLink();
-    updateURLHash();
-}
-
-
-
-
-// Event listener for navigation links
-document.querySelectorAll('a.nav-link').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        const targetIndex = Array.from(sections).findIndex(section => section.id === targetId);
-        scrollToSection(targetIndex);
+            // Scroll to the target section
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        });
     });
 });
 
-function updateActiveNavLink() {
-    const navLinks = document.querySelectorAll('a.nav-link');
-    console.log(navLinks)
-
-    navLinks.forEach(link => {
-        const sectionId = link.getAttribute('href').substring(1);
-        const section = document.getElementById(sectionId);
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.offsetHeight;
-        if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
-            link.classList.add('active');
-        } else {
-            link.classList.remove('active');
-        }
-    });
-}
-
-function updateURLHash() {
-    const currentSection = sections[currentSectionIndex];
+function updateURLHash(index) {
+    const currentSection = sections[index];
     const sectionId = currentSection.id;
     history.replaceState(null, null, `#${sectionId}`);
 }
-
-
